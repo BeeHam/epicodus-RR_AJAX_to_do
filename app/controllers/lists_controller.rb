aa2c1e1cdfd440201fc2ce46a27597e0a9b691da
Clinton Herrick & Brigham Thompson <student@epicodus.com>
2015-10-12 11:25:29 -0700

    add create controller action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,5 +14,19 @@ class ListsController < ApplicationController
     render :new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 
 end
